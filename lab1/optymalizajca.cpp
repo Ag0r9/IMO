6c2bf8df80e9dd0a2f13ac12dbcf9f80f4bdeeca
jkabcg <jan.kabzinski@capgemini.com>
2023-03-15 14:44:00 +0100

    greedy nearest neighbour fixed

--- lab1/optymalizajca.cpp
+++ lab1/optymalizajca.cpp
@@ -41,41 +41,51 @@ double **calculate_distance (Node nodes[100]){
     return dist;
 }
 
-vector<int> nearest_neighbour(Node nodes[100], double **dist){
+vector<int> nearest_neighbour(Node nodes[100], double **dist, int start_id, int end_id){
     vector<int> not_used;
-    for (int i = 1; i < 100; i++) 
-        not_used.push_back(i);
-    
+    for (int i = 0; i < 100; i++){
+        if(i != start_id && i != end_id ) 
+            not_used.push_back(i);
+    }
     vector <int> solution;
-    solution.push_back(0);
-    int last = solution[0];
-
+    solution.push_back(start_id);
+    solution.push_back(end_id);
     while(!not_used.empty()){
-        int min_dist=INT32_MAX;
+        int min_dist = INT32_MAX;
         int min_id = -1;
+        int after_whom_in_solution = -1;
         for(auto i = not_used.begin(); i!=not_used.end();i++){
-            if(dist[last][*i] < min_dist){
-                min_id = *i;
-                min_dist = dist[last][*i];
+            for(int j = 0; j+1 < solution.size(); j++)
+                {
+                    if(dist[solution[j]][*i] + dist[*i][solution[j+1]]< min_dist){
+                        min_id = *i;
+                        min_dist = dist[solution[j]][*i] + dist[*i][solution[j+1]];
+                        after_whom_in_solution = solution[j];
+                }
             }
         }
-        solution.push_back(min_id);
-        last=min_id;
-        auto q = find(not_used.begin(),not_used.end(),min_id);
-        not_used.erase(q);
+        auto q = find(solution.begin(), solution.end(), after_whom_in_solution);
+        solution.insert(q,min_id);
+        auto r = find(not_used.begin(), not_used.end(), min_id);
+        not_used.erase(r);
     }
     return solution;
 }
 
 int main(){
+    srand( time( NULL ) );
     Node nodes[100];
     load_data(nodes, "data/kroA100.tsp");
     double** distances = calculate_distance(nodes);
     
-    vector<int> x = nearest_neighbour(nodes, distances);    
-    for(int y=0;y<100;y++)
-        cout<< x[y] <<" ";
+    int end_id, start_id = rand() % 100;
+    do{
+    end_id = rand() % 100;
+    }while(end_id == start_id);
+    vector<int> x = nearest_neighbour(nodes, distances, start_id, end_id);    
 
+    for(int i = 0; i<100;i++)
+        cout<< x[i] <<" ";
     //for(int i = 0; i < 100; i++){
     //    for(int j = 0; j < 100; j++){
     //        delete distances[i];
