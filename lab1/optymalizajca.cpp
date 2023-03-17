e6078e7c74f070b49ae0f5a571fbead5841141d5
jkabcg <jan.kabzinski@capgemini.com>
2023-03-17 16:41:12 +0100

    working greedy cycle and nearest neighbour

--- lab1/optymalizajca.cpp
+++ lab1/optymalizajca.cpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <float.h>
 using namespace std;
 
 struct Node{
@@ -42,24 +43,68 @@ double **calculate_distance (Node nodes[100]){
 }
 
 void cycle_creation(double **dist, vector<int> &not_used, vector<int> &solution){
-    int min_dist = INT32_MAX;
+    double min_dist = DBL_MAX;
     int min_id = -1;
     int after_whom_in_solution = -1;
     for(auto i = not_used.begin(); i != not_used.end(); i++){
         for(int j = 0; j+1 < solution.size(); j++)
             {
-                int w = dist[solution[j]][*i] + dist[*i][solution[j+1]];
-                if(w <= min_dist){
+                double w = dist[solution[j]][*i] + dist[*i][solution[j+1]];
+                if(w < min_dist){
                     min_id = *i;
-                    min_dist = dist[solution[j]][*i] + dist[*i][solution[j+1]];
+                    min_dist = w;
                     after_whom_in_solution = solution[j];
             }
         }
     }
-    cout<< min_id<<" "<<after_whom_in_solution <<endl;
     solution.insert(++(find(solution.begin(), solution.end(), after_whom_in_solution)), min_id);
     not_used.erase(find(not_used.begin(), not_used.end(), min_id));
 }
+
+void find_nearest(double **dist, vector<int> &not_used, vector<int> &solution){
+    double min_dist = DBL_MAX;
+    int min_id = -1;
+    int after_whom_in_solution = -1;
+    for(auto i = not_used.begin(); i != not_used.end(); i++){
+        for(int j = 0; j < solution.size(); j++)
+            {
+                double w = dist[solution[j]][*i];
+                if(w < min_dist){
+                    min_id = *i;
+                    min_dist = w;
+                    after_whom_in_solution = solution[j];
+            }
+        }
+    }
+    solution.insert(++(find(solution.begin(), solution.end(), after_whom_in_solution)), min_id);
+    not_used.erase(find(not_used.begin(), not_used.end(), min_id));
+}
+
+
+
+vector<vector<int>> nearest_neighbour(double **dist, int first_start, int second_start){
+    vector<int> not_used;
+    for (int i = 0; i < 100; i++){
+        if(i != first_start && i != second_start) 
+            not_used.push_back(i);
+    }
+    vector <int> solution1, solution2;
+    solution1.push_back(first_start);
+    while(solution1.size()<50){
+        find_nearest(dist,not_used,solution1);
+    }
+    solution2.push_back(first_start);
+    while(solution2.size()<50){
+        find_nearest(dist, not_used, solution2);
+    }
+    solution2.push_back(second_start);
+
+    vector<vector<int>> x;
+    x.push_back(solution1);
+    x.push_back(solution2);
+    return x;
+}
+
 
 vector<vector<int>> greedy_cycle(double **dist, int first_start, int second_start){
     vector<int> not_used;
@@ -70,12 +115,12 @@ vector<vector<int>> greedy_cycle(double **dist, int first_start, int second_star
     vector <int> solution1, solution2;
     solution1.push_back(first_start);
     solution1.push_back(first_start);
-    while(solution1.size()<6){
+    while(solution1.size()<51){
         cycle_creation(dist,not_used,solution1);
     }
     solution2.push_back(second_start);
     solution2.push_back(second_start);
-    while(solution2.size()<1){
+    while(solution2.size()<51){
         cycle_creation(dist, not_used, solution2);
     }
     vector<vector<int>> x;
@@ -91,19 +136,32 @@ int main(){
     double** distances = calculate_distance(nodes);
     
     int second_id, first_id = rand() % 100;
-    do{
-    second_id = rand() % 100;
-    }while(second_id == first_id);
+    double max_dist=0.0;
+    for(int i=0;i<100;i++){
+        if(distances[i][first_id] > max_dist){
+            second_id = i;
+            max_dist = distances[i][first_id];
+        }
+    }
     vector<vector<int>> x = greedy_cycle(distances, first_id, second_id);    
 
-    for(int i = 0; i<6;i++)
-        cout<< x[0][i] <<" ";
+    double d =0.0;
+    for(int i = 0; i<50;i++)
+        d+=distances[x[0][i]][x[0][i+1]];
 
-    //for(int i = 0; i < 100; i++){
-    //    for(int j = 0; j < 100; j++){
-    //        delete distances[i];
-    //    }
-    //}    
-    //delete distances;
+    for(int i = 0; i<50;i++)
+        d+=distances[x[1][i]][x[1][i+1]];
+
+    cout << "Greedy cycle: "<< d <<endl;
+
+    vector<vector<int>> y = nearest_neighbour(distances, first_id, second_id); 
+    int d2=0.0;
+    for(int i = 0; i<50;i++)
+        d2+=distances[y[0][i]][x[0][i+1]];
+
+    for(int i = 0; i<50;i++)
+        d2+=distances[y[1][i]][x[1][i+1]];
+
+    cout <<"Nearest neighbour: " << d2 <<endl;
     return 0;
 }
