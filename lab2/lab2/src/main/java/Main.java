5e2cf422db4cb5379b7e4a61d0e558edc6b84e14
jkabzins <OjJanusz2>
2023-03-28 15:27:24 +0200

    refactor

--- lab2/lab2/src/main/java/Main.java
+++ lab2/lab2/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.io.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Main {
     static class Node {
@@ -57,7 +59,7 @@ class Main {
         not_used.remove(Integer.valueOf(min_id));
     }
 
-    static ArrayList<Integer>[] greedy_cycle(double[][] dist, int first_start, int second_start) {
+    static ArrayList<Integer>[] generate_greedy_cycles(double[][] dist, int first_start, int second_start) {
         ArrayList<Integer> not_used = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             if (i != first_start && i != second_start)
@@ -123,15 +125,54 @@ class Main {
                 if (cost < 0) {
                     first_cycle.set(i, j_value);
                     second_cycle.set(j, i_value);
-
-                    ArrayList<Integer> x[] = new ArrayList[]{first_cycle, second_cycle};
-                    return x;
+                    return new ArrayList[]{first_cycle, second_cycle};
                 }
             }
         }
-        ArrayList<Integer> x[] = new ArrayList[]{first_cycle, second_cycle};
-        return x;
+        return new ArrayList[]{first_cycle, second_cycle};
     }
+
+    static ArrayList<Integer>[] steep_vertex_exchange(
+            double[][] dist, ArrayList<Integer> first_cycle, ArrayList<Integer> second_cycle) {
+        double min_cost = Double.MAX_VALUE;
+        int i_idx_to_switch = -1, j_idx_to_switch = -1;
+        int are_cycles_the_same = first_cycle.equals(second_cycle) ? 1 : -1;
+        //jesli działamy wewnątrz jednego cyklu, to nie porównuj wierzchołków oddalonych
+        //o mniej niż dwa, bo już są ze sobą połączone. Jesli są z innych cykli omin warunek,
+        //gdyz abs > -2
+        for (int i = 1; i < first_cycle.size() - 1; i++) {
+            for (int j = 1; j < second_cycle.size() - 1; j++) {
+                if (Math.abs(i - j) < 2 * are_cycles_the_same)
+                    continue;
+
+                int i_prev = first_cycle.get(i - 1);
+                int i_value = first_cycle.get(i);
+                int i_next = first_cycle.get(i + 1);
+
+                int j_prev = second_cycle.get(j - 1);
+                int j_value = second_cycle.get(j);
+                int j_next = second_cycle.get(j + 1);
+
+                double cost =
+                        (dist[i_prev][j_value] + dist[j_value][i_next] +
+                                dist[j_prev][i_value] + dist[i_value][j_next]) -
+                                (dist[i_prev][i_value] + dist[i_value][i_next] +
+                                        dist[j_prev][j_value] + dist[j_value][j_next]);
+
+                if (cost < 0 && min_cost > cost) {
+                    min_cost = cost;
+                    i_idx_to_switch = i;
+                    j_idx_to_switch = j;
+                }
+            }
+        }
+        if (min_cost != Double.MAX_VALUE) {
+            first_cycle.set(i_idx_to_switch, second_cycle.get(j_idx_to_switch));
+            second_cycle.set(j_idx_to_switch, first_cycle.get(i_idx_to_switch));
+        }
+        return new ArrayList[]{first_cycle, second_cycle};
+    }
+
 
     static ArrayList<Integer> greedy_edge_exchange(double[][] dist, ArrayList<Integer> first_cycle) {
         for (int i = 0; i < first_cycle.size() - 1; i++) {
@@ -147,16 +188,50 @@ class Main {
 
                 double cost = (dist[i_value][j_value] + dist[i_next][j_next]) - (dist[i_value][i_next] + dist[j_value][j_next]);
                 if (cost < 0) {
-                    Collections.reverse(first_cycle.subList(i + 1, j+1));
+                    Collections.reverse(first_cycle.subList(i + 1, j + 1));
                     return first_cycle;
                 }
             }
         }
+        return first_cycle;
+    }
+
+    static ArrayList<Integer> steep_edge_exchange(double[][] dist, ArrayList<Integer> first_cycle) {
+        double min_cost = Double.MAX_VALUE;
+        int i_idx_to_reverse = -1, j_idx_to_reverse = -1;
+
+        for (int i = 0; i < first_cycle.size() - 1; i++) {
+            for (int j = 0; j < first_cycle.size() - 1; j++) {
+                if (Math.abs(i - j) < 3)
+                    continue;
+
+                int i_value = first_cycle.get(i);
+                int i_next = first_cycle.get(i + 1);
+
+                int j_value = first_cycle.get(j);
+                int j_next = first_cycle.get(j + 1);
+
+                double cost = (dist[i_value][j_value] + dist[i_next][j_next]) - (dist[i_value][i_next] + dist[j_value][j_next]);
+                if (cost < 0 && min_cost > cost) {
+                    min_cost = cost;
+                    i_idx_to_reverse = i + 1;
+                    j_idx_to_reverse = j + 1;
+                }
+            }
+        }
+        if (min_cost != Double.MAX_VALUE)
+            Collections.reverse(first_cycle.subList(i_idx_to_reverse, j_idx_to_reverse));
 
         return first_cycle;
     }
 
+
     void print_results(double distances[][], ArrayList[] x) {
+        for (int i = 0; i < 300; i++) {
+            x[0] = greedy_edge_exchange(distances, x[0]);
+            x[1] = greedy_edge_exchange(distances, x[1]);
+        }
+        count_result(distances, x, "edges");
         var y = x;
         int q = 0;
         var w = y;
@@ -210,6 +285,35 @@ class Main {
         System.out.println("Greedy cycle, exchange between:" + d);
     }
 
+    static void count_result(double distances[][], ArrayList<Integer>[] cycles, String type) {
+        double total_dist = 0.0;
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 50; i++) {
+                total_dist += distances[cycles[j].get(i)][cycles[j].get(i + 1)];
+            }
+        }
+        System.out.println("Greedy cycle: " + type + "  " + total_dist);
+    }
+    static ArrayList<Integer>[] generate_random_cycles(int first_id, int second_id){
+        List<Integer> not_used = IntStream.range(0, 100).filter(i-> i!=first_id && i!=second_id).boxed().collect(Collectors.toList());
+        ArrayList<Integer> first_cycle = new ArrayList<>(){{add(first_id);}};
+        ArrayList<Integer> second_cycle = new ArrayList<>(){{add(second_id);}};
+
+        Random rand = new Random();
+        while(first_cycle.size()<50){
+            int idx =rand.nextInt(not_used.size());
+            first_cycle.add(idx);
+            not_used.remove(idx);
+        }
+        first_cycle.add(first_id);
+        while(second_cycle.size()<50){
+            int idx =rand.nextInt(not_used.size());
+            second_cycle.add(idx);
+            not_used.remove(idx);
+        }
+        second_cycle.add(first_id);
+        return new ArrayList[]{first_cycle,second_cycle};
+    }
     public static void main(String[] args) throws IOException {
         Node[] nodes = new Node[100];
         load_data(nodes, "kroA100.tsp");
@@ -218,40 +322,13 @@ class Main {
         Random rand = new Random();
         int first_id = rand.nextInt(100);
         int second_id = find_second_starting_node(first_id, distances);
-//        ArrayList<Integer> x[] = new ArrayList[2];
-//        x[0] = new ArrayList<>();
-//        x[1] = new ArrayList<>();
-//        for (int i = 0; x[0].size() <= 50; i++) {
-//            if (i != first_id)
-//                x[0].add(i);
-//        }
-//        for (int i = 0; x[1].size() <= 50; i++) {
-//            if (i != second_id)
-//                x[1].add(i);
-//        }
-        ArrayList<Integer> x[] = greedy_cycle(distances, first_id, second_id);
 
-        var y = x;
-        double d = 0.0;
-        for (int j = 0; j < 2; j++) {
-            for (int i = 0; i < 50; i++) {
-                d += distances[y[j].get(i)][y[j].get(i + 1)];
-            }
-        }
-        System.out.println("Greedy cycle:             " + d);
+        //ArrayList<Integer> cycles[] =generate_random_cycles(first_id,second_id);
+        ArrayList<Integer> cycles[] = generate_greedy_cycles(distances, first_id, second_id);
 
-        for (int i = 0; i < 300; i++) {
-            y[0] = greedy_edge_exchange(distances, y[0]);
-            y[1] = greedy_edge_exchange(distances, y[1]);
-        }
-        d = 0.0;
-        for (int j = 0; j < 2; j++) {
-            for (int i = 0; i < 50; i++) {
-                d += distances[y[j].get(i)][y[j].get(i + 1)];
-            }
+        count_result(distances, cycles, "initial");
 
-        }
-        System.out.println("Greedy cycle: edges       " + d);
+
 
     }
 }
